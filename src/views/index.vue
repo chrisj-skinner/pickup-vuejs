721f43a8df6b13945c41b5d3248f47a9c35e2743
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-05 23:24:33 +0100

    :tada: :art: Init Commit and markup setup

---
+++ src/views/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="has-text-centered">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1 class="title is-size-1 has-text-weight-normal is-spaced">..pickup..</h1>
+    <h2 class="subtitle is-size-3 has-text-weight-normal">Pickup app built with vuejs</h2>
+    <div class="content">
+      <div class="links">
+        <router-link :to="{ name: 'create'}" class="button is-link is-outlined">List an item</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
