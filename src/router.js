5578fb00eb19a7c3aa072297048bbb668d9d693c
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-05 23:25:56 +0100

    :tada: Init Commit :rotating_light:

--- src/router.js
+++ src/router.js
@@ -1,26 +1,26 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import index from './views/index.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: '/',
+      name: 'index',
+      component: index
     },
     {
-      path: "/about",
-      name: "about",
+      path: '/create',
+      name: 'create',
       // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
+      // this generates a separate chunk (create.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "create" */ './views/create.vue')
     }
   ]
-});
+})
