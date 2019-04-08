cd6e275e6f3f29eb44fc1917ec9419c670ae4611
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-08 19:42:06 +0100

    :tada: Init Commit

---
+++ src/components/Base/BaseIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="../../assets/logo.png" :width="width" :height="height">
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
