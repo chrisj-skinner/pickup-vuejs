26f57f45417b07fcf8f838a3cc14fe06e315dab9
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-08 19:48:22 +0100

    :tada:

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="level has-border-primary no-border-bottom is-marginless">
+    <div class="level-left">
+      <BaseIcon name="logo.82b9c7a5" alt="Vue logo" class="level-item"/>
+      <h1 class="is-size-4 level-item is-family-code has-text-weight-semibold is-uppercase">pickup</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
