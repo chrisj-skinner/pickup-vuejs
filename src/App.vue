6d249c4f5a0eaa8f172314a2d08b41bde5e1fab8
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-08 19:43:45 +0100

    :sparkles: Add new BaseIcon component

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,16 @@
   <div id="app">
     <div class="columns is-centered">
       <div class="column is-half">
+        <header class="level has-border-primary no-border-bottom is-marginless">
+          <div class="level-left">
+            <BaseIcon name="logo.82b9c7a5" alt="Vue logo" class="level-item"/>
+            <h1
+              class="is-size-4 level-item is-family-code has-text-weight-semibold is-uppercase"
+            >pickup</h1>
+          </div>
+        </header>
         <!-- eslint-disable-next-line -->
-        <router-view class="has-border-primary"/>
+        <router-view class="has-border-primary no-border-top no-border-bottom"/>
         <!-- eslint-disable-next-line -->
         <NavBar/>
       </div>
@@ -21,10 +29,23 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
 .has-border-primary {
-  border-width: 1px 1px 0px 1px;
-  border-style: solid;
-  border-color: #dbdbdb;
+  border: 1px solid #dbdbdb;
+}
+// Lets go and save 4 lines of code with this mixin -- Woooooooo!
+@mixin noBorder() {
+  // use .no-border-top to remove border
+  @each $item in 'top', 'right', 'left', 'bottom' {
+    .no-border-#{$item} {
+      border-#{$item}: 0;
+    }
+  }
+}
+@include noBorder();
+</style>
+<style scoped>
+header {
+  padding: 0.8rem;
 }
 </style>
