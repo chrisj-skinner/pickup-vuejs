5c6f1980098d66120134c3ef7dba403f9c594133
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-08 17:05:36 +0100

    :tada: Init Commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="tabs is-toggle is-fullwidth">
+    <ul>
+      <li class="is-active">
+        <router-link :to="{ name: 'index' }">
+          <span>Home</span>
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'create' }">
+          <span>List an item</span>
+        </router-link>
+      </li>
+      <li>
+        <a>
+          <span>Videos</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
