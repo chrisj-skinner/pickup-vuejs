9711e1f7fc9a73e4291a04f3a344b8046cd3cbb3
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-08 19:48:43 +0100

    :art: Markup tidy

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-    <router-link :to="{ name: 'id', params: { id: '1' } }">
-      <article class="box">
-        <figure class="image is-square">
-          <img src="https://bulma.io/images/placeholders/128x128.png">
-        </figure>
-        <h4 class="has-text-left">Title for item 1 (make dynamic)</h4>
-        <p class="has-text-left is-size-7">date @time</p>
-      </article>
-    </router-link>
-  </div>
+  <router-link :to="{ name: 'id', params: { id: '1' } }">
+    <article class="box">
+      <figure class="image is-square">
+        <img src="https://bulma.io/images/placeholders/128x128.png">
+      </figure>
+      <h4 class="has-text-left">Title for item 1 (make dynamic)</h4>
+      <p class="has-text-left is-size-7">date @time</p>
+    </article>
+  </router-link>
 </template>
 
 <script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,19 @@
 <template>
   <nav class="tabs is-toggle is-fullwidth">
     <ul>
-      <li class="is-active">
-        <router-link :to="{ name: 'index' }">
+      <li>
+        <router-link :to="{ name: 'index' }" class="is-radiusless">
           <span>Home</span>
         </router-link>
       </li>
       <li>
-        <router-link :to="{ name: 'create' }">
+        <router-link :to="{ name: 'create' }" class="is-radiusless">
           <span>List an item</span>
         </router-link>
       </li>
     </ul>
   </nav>
 </template>
+
+<style scoped>
+</style>
