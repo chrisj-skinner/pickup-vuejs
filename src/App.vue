6e67a78837a14edcd98317c1e801af7447ce72a9
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-08 17:05:11 +0100

    :art: Add NavBar - classes and styles

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,29 @@
   <div id="app">
     <div class="columns is-centered">
       <div class="column is-half">
-        <router-view/>
+        <!-- eslint-disable-next-line -->
+        <router-view class="has-border-primary"/>
+        <!-- eslint-disable-next-line -->
+        <NavBar/>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style scoped>
+.has-border-primary {
+  border-width: 1px 1px 0px 1px;
+  border-style: solid;
+  border-color: #dbdbdb;
+}
+</style>
