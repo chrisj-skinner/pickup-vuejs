273048ceff70e97e09134b0e820c2384366beae9
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-08 17:06:05 +0100

    :fire: Move markup to NavBar component

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,11 +3,6 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <h1 class="title is-size-1 has-text-weight-normal is-spaced">..pickup..</h1>
     <h2 class="subtitle is-size-3 has-text-weight-normal">Pickup app built with vuejs</h2>
-    <div class="content">
-      <div class="links">
-        <router-link :to="{ name: 'create'}" class="button is-link is-outlined">List an item</router-link>
-      </div>
-    </div>
     <div class="columns">
       <ItemList class="column is-half"/>
     </div>
