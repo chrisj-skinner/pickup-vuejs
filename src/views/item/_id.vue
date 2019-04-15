fcc866439badf24207b7453f922b42872c60a04b
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-15 23:00:42 +0100

    :art: Add service call and update markup

--- src/views/item/_id.vue
+++ src/views/item/_id.vue
@@ -1,22 +1,57 @@
 <template>
-  <section class="section">
+  <section>
     <article>
-      <figure class="image is-square">
-        <img src="https://bulma.io/images/placeholders/128x128.png">
-      </figure>
-      <h4 class="has-text-left">Title for item #{{ id }}</h4>
-      <p class="has-text-left is-size-7">date @time</p>
-      <p class="has-text-left">Some description here</p>
+      <div class="tile is-ancestor">
+        <div class="tile is-vertical is-3">
+          <div class="tile">
+            <div class="tile is-parent is-vertical">
+              <div class="tile is-child box notification is-info"></div>
+              <div class="tile is-child box notification is-warning"></div>
+              <div class="tile is-child box notification is-danger"></div>
+              <div class="tile is-child box notification is-info"></div>
+            </div>
+          </div>
+        </div>
+        <div class="tile is-parent">
+          <div class="tile is-child box notification is-primary has-text-centered">
+            <!-- <figure class="image is-square">
+            <img src="https://bulma.io/images/placeholders/128x128.png">
+            </figure>-->
+          </div>
+        </div>
+      </div>
+      <header>
+        <h1 class="has-text-left title is-2">{{ item.title }}</h1>
+        <p class="has-text-left subtitle is-4">Listed: {{ item.date }} @{{item.time}}</p>
+      </header>
+      <hr>
+      <p class="has-text-left is-size-6">{{ item.description }}</p>
     </article>
   </section>
 </template>
 
 <script>
+import ItemService from '@/services/ItemService.js'
+
 export default {
   props: {
     id: {
-      type: Number
+      type: String | Number
     }
+  },
+  data() {
+    return {
+      item: {}
+    }
+  },
+  created() {
+    ItemService.getItem(this.id)
+      .then(response => {
+        this.item = response.data
+      })
+      .catch(error => {
+        console.log('There was an error getting item #: ' + this.id)
+      })
   }
 }
 </script>
