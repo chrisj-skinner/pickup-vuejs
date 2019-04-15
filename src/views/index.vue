4b3dd4990af742b645076e4df066caa6929f056f
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-15 21:46:38 +0100

    :art: Add service call and for loop

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,17 +1,34 @@
 <template>
   <div class="has-text-centered section">
     <div class="columns">
-      <ItemList class="column is-half"/>
+      <ItemCard v-for="item in items" :key="item.id" :item="item" class="column is-half"/>
     </div>
   </div>
 </template>
 
 <script>
-import ItemList from '@/components/ItemList.vue'
+import ItemCard from '@/components/ItemCard.vue'
+import ItemService from '@/services/ItemService.js'
 
 export default {
   components: {
-    ItemList
+    ItemCard,
+    ItemService
+  },
+  data() {
+    return {
+      items: []
+    }
+  },
+  created() {
+    ItemService.getItems()
+      .then(response => {
+        console.log(response.data)
+        this.items = response.data
+      })
+      .catch(error => {
+        console.log('There was an error: ' + error.response)
+      })
   }
 }
 </script>
