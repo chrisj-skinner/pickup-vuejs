ab5ce0257146aaebfedebbcb000fadea9bdccea4
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-15 23:01:50 +0100

    :art:

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <router-link :to="{ name: 'id', params: { id: item.id } }">
-      <article class="box">
+      <article>
         <figure class="image is-square">
-          <!-- eslint-disable-next-line -->
           <img :src="image.src" :width="image.width" :height="image.height">
         </figure>
-        <h4 class="has-text-left">{{ item.title}}</h4>
-        <p class="has-text-left is-size-7">Listed: {{ item.date }} @{{item.time}}</p>
+        <div class="box notification is-info"></div>
+        <header>
+          <h1 class="has-text-left title is-5">{{ item.title}}</h1>
+          <p class="has-text-left subtitle is-7">Listed: {{ item.date }} @{{item.time}}</p>
+        </header>
       </article>
     </router-link>
   </div>
