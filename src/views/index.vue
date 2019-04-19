ffce1c88990b3fa099013b91ab991d532d923f46
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-19 22:34:18 +0100

    :art: Add: - pagination markup - helper functions

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,6 +3,20 @@
     <div class="columns item-card-wrap">
       <ItemCard v-for="item in items" :key="item.id" :item="item" class="column is-half item-card"/>
     </div>
+    <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+      <router-link
+        :to="{ name: 'index', query: { page: page - 1 }}"
+        rel="prev"
+        class="pagination-previous"
+        :disabled="prevDisabled"
+      >Previous</router-link>
+      <router-link
+        :to="{ name: 'index', query: { page: page + 1 }}"
+        rel="prev"
+        class="pagination-next"
+        :disabled="nextDisabled"
+      >Next Page</router-link>
+    </nav>
   </div>
 </template>
 
@@ -15,9 +29,20 @@ export default {
     ItemCard
   },
   created() {
-    this.$store.dispatch('fetchItems')
+    this.$store.dispatch('fetchItems', { perpage: 2, page: this.page })
   },
-  computed: mapState(['items'])
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    prevDisabled() {
+      return this.page === 1
+    },
+    nextDisabled() {
+      return this.total <= this.page * 2
+    },
+    ...mapState(['items', 'total'])
+  }
 }
 </script>
 
@@ -25,19 +50,18 @@ export default {
 .item-card {
   padding-top: 0;
   padding-bottom: 1rem;
-  &-wrap {
-    flex-wrap: wrap;
-    max-height: 400px;
-    overflow: scroll;
-    overflow-x: hidden;
-    &::-webkit-scrollbar {
-      width: 1em;
-    }
+  // &-wrap {
+  //   max-height: 400px;
+  //   overflow: scroll;
+  //   overflow-x: hidden;
+  //   &::-webkit-scrollbar {
+  //     width: 1em;
+  //   }
 
-    &::-webkit-scrollbar-thumb {
-      background-color: #00d1b2;
-      outline: none;
-    }
-  }
+  //   &::-webkit-scrollbar-thumb {
+  //     background-color: #00d1b2;
+  //     outline: none;
+  //   }
+  // }
 }
 </style>
